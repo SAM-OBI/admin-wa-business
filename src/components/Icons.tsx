2a728cda2351d6c5f106df9eec4f8d95a06b59f9
samuelnwokike27@gmail.com <samuelnwokike27@gmail.com>
2026-01-06 04:26:33 +0100

    feat: add centralized Icons.tsx for consistent icon imports

---
+++ src/components/Icons.tsx
@@ -0,0 +1,92 @@
+/**
+ * Centralized Icon Exports
+ * All icons used across the admin dashboard
+ * Import from here instead of react-icons directly
+ */
+
+// Feather Icons (Fi)
+export {
+  // Navigation
+  FiArrowLeft,
+  FiArrowRight,
+  FiMenu,
+  FiX,
+  
+  // Actions
+  FiEdit,
+  FiTrash,
+  FiSave,
+  FiUpload,
+  FiDownload,
+  FiRefreshCw,
+  FiSearch,
+  FiFilter,
+  FiEye,
+  FiEyeOff,
+  FiCopy,
+  FiExternalLink,
+  
+  // Status
+  FiCheckCircle,
+  FiXCircle,
+  FiAlertCircle,
+  FiAlertTriangle,
+  FiInfo,
+  
+  // User & Account
+  FiUser,
+  FiUsers,
+  FiMail,
+  FiPhone,
+  FiLock,
+  FiUnlock,
+  FiShield,
+  
+  // Business
+  FiPackage,
+  FiShoppingCart,
+  FiShoppingBag,
+  FiDollarSign,
+  FiCreditCard,
+  FiTrendingUp,
+  FiTrendingDown,
+  FiBarChart,
+  FiPieChart,
+  
+  // Date & Time
+  FiCalendar,
+  FiClock,
+  
+  // Files & Media
+  FiFile,
+  FiFileText,
+  FiFolder,
+  FiImage,
+  
+  // Communication
+  FiMessageSquare,
+  FiMessageCircle,
+  FiBell,
+  
+  // Settings
+  FiSettings,
+  FiSliders,
+  
+  // Misc
+  FiHome,
+  FiStar,
+  FiHeart,
+  FiFlag,
+  FiMoreVertical,
+  FiMoreHorizontal,
+  FiPlus,
+  FiMinus,
+  FiChevronDown,
+  FiChevronUp,
+  FiChevronLeft,
+  FiChevronRight,
+} from 'react-icons/fi';
+
+// Add other icon libraries as needed
+// export { ... } from 'react-icons/fa';
+// export { ... } from 'react-icons/md';
