47aeff9577f3e30f36e6311afa45a988cbcfe073
samuelnwokike27@gmail.com <samuelnwokike27@gmail.com>
2026-02-25 21:45:50 +0100

    feat(admin): consolidate production refactor - updated pages and logic

---
+++ dist/assets/logger-kjsKaDI_.js
@@ -0,0 +1,1 @@
+const e={info:(...o)=>console.log("[INFO]",...o),error:(...o)=>console.error("[ERROR]",...o),warn:(...o)=>console.warn("[WARN]",...o),debug:(...o)=>console.debug("[DEBUG]",...o)};export{e as l};
