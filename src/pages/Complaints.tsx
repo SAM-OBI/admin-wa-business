31dc0e86035701e2b4352378775dafb5e70ca82f
samuelnwokike27@gmail.com <samuelnwokike27@gmail.com>
2026-05-14 17:23:09 +0100

    fix: resolve lint error (unused variable 'val') in Complaints.tsx

--- src/pages/Complaints.tsx
+++ src/pages/Complaints.tsx
@@ -95,7 +95,7 @@ export default function Complaints() {
         <div className="flex flex-col sm:flex-row gap-3">
           <HardenedSearchInput
             value=""
-            onChange={(val) => {
+            onChange={(_) => {
               // Implementation would involve adding search param to fetchComplaints
             }}
             placeholder="SEARCH ISSUES..."
